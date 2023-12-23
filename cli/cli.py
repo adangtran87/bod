85b53ed44fac466ef7da05acabf283999aefb515
Alexander Dang-Tran <alex.dangtran@gmail.com>
2023-12-22 20:47:27 -0600

    [cli] feat: add function to scan a card

--- cli/cli.py
+++ cli/cli.py
@@ -1,9 +1,12 @@
+from typing_extensions import Annotated
 import typer
 
 from asyncio import run as aiorun
 
 import bank.database.accounts as accounts
+import bank.database.cards as cards
 import bank.database.database as database
+import bank.app.nfcutils.nfcutils as nfc
 
 app = typer.Typer()
 
@@ -18,6 +21,7 @@ def init_db():
     async def _init_db():
         async with await database.get_db() as db:
             await accounts.create_table(db)
+            await cards.create_table(db)
 
     aiorun(_init_db())
 
@@ -49,6 +53,29 @@ def get_accounts():
             return await accounts.get_accounts(db)
 
     print(aiorun(_get_accounts()))
+
+
+async def _scan_card(device: str) -> str:
+    dev = nfc.NfcDevice(device)
+    print("Tap Card:")
+    await dev.connect()
+    if dev.has_data():
+        data = await dev.get_data()
+        print(data)
+        return ""
+    else:
+        return ""
+
+
+@app.command()
+def scan_card(
+    device: Annotated[
+        str, typer.Option(help="Device location string")
+    ] = "tty:USB0:pn532",
+):
+    card_id = aiorun(_scan_card(device))
+    print(card_id)
+
 
 if __name__ == "__main__":
     app()
