f88e07b67d26f88d4d4566f5f11a8417a1e91f34
Alexander Dang-Tran <alex.dangtran@gmail.com>
2023-12-23 16:09:05 -0600

    feat(cli): Add aliases and help

--- cli.py
+++ cli.py
@@ -6,10 +6,12 @@ import cli.database as database
 import cli.transaction as transaction
 
 app = typer.Typer()
-app.add_typer(account.app, name="account")
-app.add_typer(card.app, name="card")
-app.add_typer(database.app, name="db")
-app.add_typer(transaction.app, name="t")
+app.add_typer(account.app, name="a", help="Account commands")
+app.add_typer(account.app, name="account", help="Account commands")
+app.add_typer(card.app, name="card", help="Card commands")
+app.add_typer(database.app, name="db", help="Database commands")
+app.add_typer(transaction.app, name="t", help="Transaction commands")
+app.add_typer(transaction.app, name="transaction", help="Transaction commands")
 
 
 if __name__ == "__main__":
