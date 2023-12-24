789f87563a36cbb9dc93f07f9c6c39360edb5bbb
Alexander Dang-Tran <alex.dangtran@gmail.com>
2023-12-23 23:51:40 -0600

    feat(cli): Add `cli t sub`
    Cannot pass negative numbers into cli so make a command for negative
    transactions

--- cli/transaction.py
+++ cli/transaction.py
@@ -72,3 +72,49 @@ def add(
         pprint(r.ok_value)
     else:
         pprint(r.err_value)
+
+
+@app.command()
+def sub(
+    value: Annotated[float, typer.Argument(help="Value to subtract")],
+    device: Annotated[
+        str, typer.Option(help="Device location string")
+    ] = "tty:USB0:pn532",
+    note: Annotated[Optional[str], typer.Option(help="Transaction note")] = None,
+):
+    """Add a transaction "to a card"
+
+    This assumes the user has run "cli account add --scan" prior
+    to create an account associated with a card
+    """
+
+    async def _scan_and_add(
+        value: float, device: str, note: str | None
+    ) -> Result[str, str]:
+        r = await scan_card(device)
+        if isinstance(r, Ok):
+            card_id = r.ok_value
+            t = transactions.Transaction(
+                id=0, date=datetime.now(), value=-value, account_id=0, note=note
+            )
+            async with await database.get_db() as db:
+                t_id = await transactions.add_transaction_from_card_id(
+                    db, card_id=card_id, t=t
+                )
+                result = {
+                    "id": t_id,
+                    "value": -value,
+                    "note": note,
+                }
+                if t_id:
+                    return Ok(f"Created transaction {result}")
+                else:
+                    return Err(f"No account registered to card: {card_id}")
+        else:
+            return Err(r.err_value)
+
+    r = aiorun(_scan_and_add(value, device, note))
+    if isinstance(r, Ok):
+        pprint(r.ok_value)
+    else:
+        pprint(r.err_value)
